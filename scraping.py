705cd64f1072ac6e0e5a8e03d166268070724cda
Mason Russell <mason.russell@okstate.edu>
2020-09-01 23:56:34 -0500

    Adds in functionality for parsing wikipedia and moves the scores into the Movie class

--- scraping.py
+++ scraping.py
@@ -3,22 +3,42 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup as BS
 
+rotten_tomatoes_url = 'https://www.rottentomatoes.com/m/'
+
 def sanatize_html_strings(text, dic):
     for i, j in dic.items():
         text = text.replace(i, j)
     return text
 
+def getHTML(url, name):
+        print('Retrieving %s HTML...\n' % name)
+        page = urlopen(url)
+        html = BS(page.read(), features='lxml')
+        print('HTML received.\n')
+        return html
+
+def wikipedia_scrape():
+    url = 'https://en.wikipedia.org/wiki/List_of_Academy_Award-winning_films'
+    return getHTML(url, 'Wikipedia')
+
+def determine_movies(html):
+    content = html.find('table', {'class' : 'wikitable sortable'})
+    movies_refs = content.find_all('i')
+    movies_list = []
+    for movie in movies_refs:
+        try:
+            title = movie.find('a').contents[0]
+            movies_list.append(title)
+        except:
+            pass
+    return movies_list
+
 class Movie:
     def __init__(self, url, name):
         self.url = url
         self.name = name
-
-    def getHTML(self):
-        print('Retrieving %s HTML...\n' % self.name)
-        page = urlopen(test_movie.url)
-        html = BS(page.read(), features='lxml')
-        print('HTML received.\n')
-        return html
+        self.critics_score = 0
+        self.audience_score = 0
 
     def findRatings(self, html):
         print('Determining ratings for %s...\n' % self.name)
@@ -39,15 +59,13 @@ class Movie:
             ' ' : '',
             '%' : ''
         }
-        critics_score = sanatize_html_strings(critics_score, items_to_replace)
-        audience_score = sanatize_html_strings(audience_score, items_to_replace)
+        self.critics_score = sanatize_html_strings(critics_score, items_to_replace)
+        self.audience_score = sanatize_html_strings(audience_score, items_to_replace)
         print('Ratings Determined.\n')
-        return {
-            'critics': critics_score, 
-            'audience': audience_score
-        }
 
-test_movie = Movie(url='https://www.rottentomatoes.com/m/learning_to_skateboard_in_a_warzone', name='Star Trek Beyond')
-bs_html = test_movie.getHTML()
-scores = test_movie.findRatings(bs_html)
-print(scores)
+wiki_html = wikipedia_scrape()
+movies = determine_movies(wiki_html)
+test_movie = Movie(url=rotten_tomatoes_url + 'learning_to_skateboard_in_a_warzone', name='Star Trek Beyond')
+bs_html = getHTML(test_movie.url, test_movie.name)
+test_movie.findRatings(bs_html)
+print('%s, %s' % (test_movie.critics_score, test_movie.audience_score))
