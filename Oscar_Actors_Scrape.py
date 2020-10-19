3582df440731121b33ef70e89f01e32a36bad917
RyanShaffer <ryan.r.shaffer@okstate.edu>
2020-10-19 13:48:31 -0500

    Oscar Actors and Best Picture

--- Oscar_Actors_Scrape.py
+++ Oscar_Actors_Scrape.py
@@ -4,7 +4,6 @@ from lxml import etree
 import requests
 import regex
 
-
 Oscar_Actors_data = []
 #Pull in the website html
 for i in range(1,3):
@@ -16,5 +15,11 @@ for i in range(1,3):
     Actors = '//h3[@class="lister-item-header"]/a/text()'
     Actors = oscar_tree.xpath(Actors)
     Oscar_Actors_data.append(Actors)
-
-Oscar_Actors_data = 
+#Clean the data
+Oscar_Actors_data0 = ''.join(Oscar_Actors_data[0])
+Oscar_Actors_data1 = ''.join(Oscar_Actors_data[1])
+Oscar_Actors_data = Oscar_Actors_data0 + Oscar_Actors_data1
+Oscar_Actors_data = regex.findall("([A-zàâçéèêëîïôûùüÿñæœ'.-]+|[A-zàâçéèêëîïôûùüÿñæœ'.-]+ [A-zàâçéèêëîïôûùüÿñæœ'.-]+|[A-zàâçéèêëîïôûùüÿñæœ'.-]+ [A-zàâçéèêëîïôûùüÿñæœ'.-]+ [A-zàâçéèêëîïôûùüÿñæœ'.-]+)\\n", Oscar_Actors_data)
+#Convert to a dataframe and export to csv
+Oscar_Actors_data = pd.DataFrame({'Oscar Winning Acting' : Oscar_Actors_data})
+Oscar_Actors_data.to_csv(r'C:\Users\Rrsha\source\repos\msis5193_project\Oscar_Actors.csv', index = False)
