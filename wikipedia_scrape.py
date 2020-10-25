a05e6a12385edcfa9cf3eb13ac91c298595a5451
Mason S. Russell <30694747+msrusse@users.noreply.github.com>
2020-10-25 16:20:39 -0500

    Msrusse (#12)
    * Removes unnecessary spacing
    
    * Adding additional file types to not be tracked
    
    * Adding Individual Box Office Movie Scrape Script to tracking
    
    * Small tweaks and name changes to scripts
    
    * Converts numbers into floats
    
    * Adds a sleep call in order to prevent DDOSing the boxofficemojo site. Also adds error.json outputting for pages without a 200 return.
    
    * Adds a function for attempting to get errored pages.
    
    * Adds *.log to .gitignore for new logging file
    
    * Moves function calls around to be more readable
    
    * Fixes issue caused when splitting async calls into stacks of 100
    
    * Reverts box office scrape to all years over test years.
    
    * Adds method for retrying failed movies and corrects log file output
    
    * Renames JSON keys to fit with Google Style Guide
    
    * Adds comments and alters a dictionary structure to work correctly
    
    * Msrusse (#8)
    
    * Removes unnecessary spacing
    
    * Adding additional file types to not be tracked
    
    * Adding Individual Box Office Movie Scrape Script to tracking
    
    * Small tweaks and name changes to scripts
    
    * Converts numbers into floats
    
    * Adds a sleep call in order to prevent DDOSing the boxofficemojo site. Also adds error.json outputting for pages without a 200 return.
    
    * Adds a function for attempting to get errored pages.
    
    * Adds *.log to .gitignore for new logging file
    
    * Moves function calls around to be more readable
    
    * Fixes issue caused when splitting async calls into stacks of 100
    
    * Reverts box office scrape to all years over test years.
    
    * Adds method for retrying failed movies and corrects log file output
    
    * Updates JSON formatting
    
    * Adds script to write JSON to CSV
    
    * Removes write to CSV from this script
    
    * Adds in the functionality to pull total awaards and nominations
    
    * Changes storage from dicts to list of dicts. Helps with parsing.
    
    * Adds code comments
    
    * Removes unnecessary scripts

--- wikipedia_scrape.py
+++ wikipedia_scrape.py
@@ -6,102 +6,156 @@ import json, requests, sys
 from datetime import datetime
 from progressbar import ProgressBar as pb
 
+# Sets base URL for wikipedia
 wikipedia_url = 'https://en.wikipedia.org'
+# Creates errors dictionary
 errors = {}
 # Sets the log file location
 log = open("logs/wikipedia_scrape.log", "a")
 # Sets the standard output to console
 primary_stdout = sys.stdout
 
+# Sanitizes scraped columns for awards and nominations to convert them to ints
 def sanitizeCols(cols):
     sanitized = []
+    # Go through each item in the list passed 
     for col in cols:
-        text = col.getText().split(' ')
-        text = text[0].split('[')
-        text = text[0].split('/')
-        text = text[0].replace(u'\u2013', u'-').replace('\n', '').replace(',', '').replace(' ', '')
+        # Get the first number in each column, if it has a special note and replace any special characters
+        text = col.getText().split(' ')[0].split('[')[0].split('/')[0].replace(u'\u2013', u'-').replace('\n', '').replace(',', '').replace(' ', '')
+        # Append sanitized text to the list to return as an int
         sanitized.append(int(text))
     return sanitized
 
+# Get information for the Academy Award Winning movies
 def determineMovies(response):
+    # Convert the response to a soup object
     html = BS(response.content, 'lxml')
+    # Find the table on the page
     movies_table = html.find('table', {'class': 'wikitable sortable'})
+    # Get the body from the table
     movies_table_body = movies_table.find('tbody')
+    # Get all rows from the table, each row corresponds to an award winning movie
     movie_rows = movies_table_body.find_all('tr')
+    # Creates the movies dictionary
     movies = {}
-    total_award_rows = 0
-    total_nominations_rows = 0
-    award_row = 0
-    nominations_row = 0
-    current_year = None
-    current_awards = None
-    current_nominations = None
+    # Creates variables to work with the logic for row and column spans of the awards and nominations
+    total_award_rows = total_nominations_rows = nominations_row = award_row = 0
+    current_year = current_awards = current_nominations = None
+    # Creates the progressbar object
     bar = pb()
+    # Loops through each movie, starts with index 1 because index 0 is the header row
     for movie in bar(movie_rows[1:]):
+        # Finds all table data in the row, each item corresponds to the title, year, awards, and nominations
         cols = movie.find_all('td')
+        # Gets the title and sanitizes it
         title = cols[0].getText().replace('\n', '')
+        # Gets the a tag from each row, this has the link to the movies wikipedia page
         a_ref = cols[0].find('a')
+        # Determines if an a tag exists in the row
         if a_ref:
+            # Grabs the href if the a tag exists
             wiki_link = a_ref['href']
+        # Determines if the row has a style attribute, only best picture movies have a style
         if movie.has_attr('style'):
+            # Creates and sets best picture to True when style is present
             best_picture = True
         else:
+            # Creates best picture set to False when style not present
             best_picture = False
+        # If there are more than one column, do logic to get the year, awards, and nominations. Title is always the first column and sometimes will be the only
         if len(cols) > 1:
+            # Sanitizes all numeric columns
             sanitized_cols = sanitizeCols(cols[1:])
+            # Determines if the sanitized columns contains the year, award, and nominations
             if len(sanitized_cols) == 3:
+                # Determines if the awards spans additional rows
                 if cols[2].has_attr('rowspan'):
+                    # Sets the value of total_award_rows to the amount of rows the awards span
                     total_award_rows = int(cols[2]['rowspan'])
-                    award_row = 0
+                    # Sets the value of award_row to one, noting this is the first row of the span
+                    award_row = 1
+                # Determines if the nominations spans additional rows
                 if cols[3].has_attr('rowspan'):
+                    # Sets the value of total_nominations_rows to the amount of rows the nominations span
                     total_nominations_rows = int(cols[3]['rowspan'])
-                    nominations_row = 0
+                    # Sets the value of nominations_row to one, noting this is the first row fo the nominations
+                    nominations_row = 1
+                # Sets the current year value to the year in the row
                 current_year = sanitized_cols[0]
+                # Sets the current awards to the number in the current row
                 current_awards = sanitized_cols[1]
+                # Sets the current nominations to the number in the current row
                 current_nominations = sanitized_cols[2]
+            # Determines if the sanitized columns have awards and nominations. If there are only two values in the sanitized, it will always be awards and nominations
             elif len(sanitized_cols) == 2:
+                # Determines if awards spans multiple rows
                 if cols[1].has_attr('rowspan'):
+                    # Sets total award rows to the value of the rowspan
                     total_award_rows = int(cols[1]['rowspan'])
+                    # Resets award row to 1, denoting this is the first row of the span
                     award_row = 1
                 else:
+                    # If there is no rowspan, then increment which row we are on
                     award_row += 1
+                # Determines if nominations spans multiple rows
                 if cols[2].has_attr('rowspan'):
+                    # Sets the total nominations to the row span
                     total_nominations_rows = int(cols[2]['rowspan'])
+                    # Resets current nominations row to 1
                     nominations_row = 1
                 else: 
+                    # Increments what nomination row we are on
                     nominations_row += 1
                 current_awards = sanitized_cols[0]
                 current_nominations = sanitized_cols[1]
             else:
-                row_span = 0
-                col_span = 0
+                # Sets row and col spans to 0
+                row_span = col_span = 0
+                # awards and noms is used if the number spreds across both columns and rows
                 awards_and_noms = False
+                # Determines if the value spans rows
                 if cols[1].has_attr('rowspan'):
+                    # Sets the row_span to the total number of rows spanned
                     row_span = int(cols[1]['rowspan'])
+                # Determines if the value spans columns
                 if cols[1].has_attr('colspan'):
+                    # Sets the col_span to how many columns the number spans
                     col_span = int(cols[1]['colspan'])
+                    # Sets the value to denote that the number is in both rows and columns
                     awards_and_noms = True
                 if awards_and_noms:
-                    current_awards = sanitized_cols[0]
-                    current_nominations = sanitized_cols[0]
-                    total_nominations_rows = col_span
-                    total_award_rows = col_span
-                    nominations_row = 1
-                    award_row = 1
+                    # Set the current award and nominations value to the rows number
+                    current_awards = current_nominations = sanitized_cols[0]
+                    # Sets the nominations and rows spans to the amount it spans
+                    total_nominations_rows = total_award_rows = col_span
+                    # Resets the nominations and award rows to one
+                    nominations_row = award_row = 1
+                # Determines if the current award row is less than the total rows it spans
                 elif award_row < total_award_rows:
+                    # Increments the what the current award_row is
                     award_row += 1
+                    # Sets the total nominations to the row_span value, because it is the field that changed
                     total_nominations_rows = row_span
+                    # Resets nominations row to the first row
                     nominations_row = 1
+                    # Changes the current_nominations to the amount spanning rows
                     current_nominations = sanitized_cols[0]
                 else:
+                    # Increments the nominations row
                     nominations_row += 1
+                    # Sets the amount of rows the value spans
                     total_award_rows = row_span
+                    # Resets the awards row count
                     award_row = 1
+                    # Sets the new value of awards
                     current_awards = sanitized_cols[0]
         else:
+            # Increment award and nominations rows, because neither changed
             award_row += 1
             nominations_row += 1
+        # Creates the key for the movie as the unique URL for the fild
         movie_key = wikipedia_url + wiki_link
+        # Adds the current movie to the movies dict
         movies[movie_key] = {
             'title' : title, 
             'wikiLink' : movie_key,
@@ -113,7 +167,8 @@ def determineMovies(response):
     return movies
 
 def getMoviesOnWikipedia(movies):
-    response = (grequests.get(movies[movie]['wikiLink']) for movie in movies)
+    # Async request for all the movies, based off the key, which is their URL
+    response = (grequests.get(movie) for movie in movies)
     return grequests.map(response)
 
 def checkStatusCode(response):
@@ -138,18 +193,27 @@ def checkStatusCode(response):
         print('%s' % response)
 
 def parseWikipediaPages(response):
+    # Converts response to soup object
     response_bs = BS(response.text, features='lxml')
+    # Finds all hrefs
     hrefs = response_bs.find_all('a', href=True)
+    # Sets the value for the rotten tomatoes link
     rotten_tomatoes_links = 'N/A'
+    # Loops through each href on the page
     for url in hrefs:
+        # Determines if rottentomatoes has an href link
         if 'rottentomatoes.com' in url.get('href') and 'web.archive.org' not in url.get('href'):
+            # Changes the rotten tomatoes link to the variable
             rotten_tomatoes_links = url.get('href')
     return rotten_tomatoes_links
 
 def determineIfValidRepsonse(response):
+    # Determines if a valid response code
     if response.status_code == 200:
         return True
+    # If the repsonse is a server error
     elif response.status_code == 503:
+        # Retries the request
         r = requests.get(wikipedia_url + '/wiki/List_of_Academy_Award-winning_films')
         determineIfValidRepsonse(r)
 
