2d59dcbbc173a4ba8a3da62eaba85ed2d8a0a547
Mason Russell <mason.russell@okstate.deu>
2020-12-02 17:15:26 -0600

    finishes execute script to call each script and add csv dir

--- execute.py
+++ execute.py
@@ -1,10 +1,12 @@
 #! /usr/bin/python
 
-import box_office_mojo_individual_scrape, box_office_mojo_scrape, get_imdb_info, get_movie_ratings, imdb_oscar_actors_scrape, wikipedia_scrape, write_json_to_csv
+from multiprocessing.context import Process
+from scripts import box_office_mojo_individual_scrape, box_office_mojo_scrape, get_imdb_info, get_movie_ratings, imdb_oscar_actors_scrape, wikipedia_scrape, write_json_to_csv
 import os
 
 def mkdirs():
     data_path = 'data'
+    csv_path = '%s/csv_files' % data_path
     movie_information = '%s/movie_information' % data_path
     valid_responses = '%s/valid_responses' % movie_information
     invalid_responses = '%s/invalid_responses' % movie_information
@@ -23,14 +25,43 @@ def mkdirs():
             os.mkdir(movie_summaries)
         if not os.path.isdir(movies_by_market):
             os.mkdir(movies_by_market)
+        if not os.path.isdir(csv_path):
+            os.mkdir(csv_path)
     else:
         os.mkdir(data_path)
         mkdirs()
     if not os.path.isdir('logs'):
         os.mkdir('logs')
 
+def executeBoxOfficeMojo():
+    box_office_mojo_scrape.main()
+    box_office_mojo_individual_scrape.main()
+
+def executeSinglePages():
+    wikipedia_scrape.main()
+    imdb_oscar_actors_scrape.main()
+
+def executeIMDB():
+    get_imdb_info.main()
+
+def executeRottenTomatoes():
+    get_movie_ratings.main()
+
 def main():
     mkdirs()
 
 if __name__ == '__main__':
     main()
+    p1 = Process(target=executeBoxOfficeMojo)
+    p1.start()
+    p2 = Process(target=executeSinglePages)
+    p2.start()
+    p1.join()
+    p2.join()
+    p3 = Process(target=executeIMDB)
+    p3.start()
+    p4 = Process(target=executeRottenTomatoes)
+    p4.start()
+    p3.join()
+    p4.join()
+    write_json_to_csv.main()
